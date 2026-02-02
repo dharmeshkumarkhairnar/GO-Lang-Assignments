fcb141d2c42b21368fb4614cd40e7433967788b8
dharmeshkumarkhairnar <dharmeshkumar.khairnar@coditas.com>
2026-02-02 13:47:38 +0530

    Added the comments

--- Assignment 4- Worker Pool (30-01-2026)/main.go
+++ Assignment 4- Worker Pool (30-01-2026)/main.go
@@ -8,34 +8,37 @@ import (
 
 func worker(ID int, wg *sync.WaitGroup, sharedChannel <-chan int) {
 	defer wg.Done()
-	for i := range sharedChannel {
+
+	for i := range sharedChannel { //listening to channel using range loop
 		fmt.Printf("worker Id: %d started the job: %d\n", ID, i)
-		time.Sleep(3 * time.Second)
+		time.Sleep(3 * time.Second)//waiting before execution 
 		fmt.Printf("worker Id: %d finished the job: %d\n", ID, i)
 	}
 }
 
 func main() {
 	var wg sync.WaitGroup
+	workers := 0
+	jobs := 0
 
 	fmt.Println("Enter the Total workers: ")
-	workers := 0
 	fmt.Scanln(&workers)
 
 	fmt.Println("Enter the Total Jobs: ")
-	jobs := 0
 	fmt.Scanln(&jobs)
 
 	wg.Add(workers)
+	
 	sharedChannel := make(chan int, jobs)
 
-	for i := 01; i <= workers; i++ {
-		go worker(i, &wg, sharedChannel)
+	for i := 1; i <= workers; i++ {
+		go worker(i, &wg, sharedChannel)//calling all workers as goroutine
 	}
 
-	for i := range jobs {
-		sharedChannel <- i + 1
+	for i := 1; i <= jobs; i++ { 
+		sharedChannel <- i //writing to the channel
 	}
+
 	close(sharedChannel)
 
 	wg.Wait()
